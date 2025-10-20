06f985ce17a875eb5eeaba87c253c555ac7d382d
Shlok <shlokgraman@gmail.com>
2025-10-20 15:22:08 +0530

    size changes

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -10,24 +10,24 @@ const Education: React.FC = () => {
                     <div className="education-content">
                         <img src="/images/education.png" alt="Education" />
                         <h3>X CBSE Board</h3>
-                        <p><strong>Institution:</strong> SVEI Public School</p>
-                        <p><strong>Period:</strong> Completed in 2022</p>
+                        <p>SVEI Public School</p>
+                        <p>Completed 2022</p>
                     </div>
                 </div>
                 <div className="education-item">
                     <div className="education-content">
                         <img src="/images/education.png" alt="Education" />
-                        <h3>II PU (Pre-University Course)</h3>
-                        <p><strong>Institution:</strong> RVPB PU College</p>
-                        <p><strong>Period:</strong> 2022 - 2024</p>
+                        <h3>II PU</h3>
+                        <p>RVPB PU College</p>
+                        <p>2022 - 2024</p>
                     </div>
                 </div>
                 <div className="education-item">
                     <div className="education-content">
                         <img src="/images/education.png" alt="Education" />
-                        <h3>Bachelor of Engineering in Information Science</h3>
-                        <p><strong>Institution:</strong> RV College of Engineering</p>
-                        <p><strong>Period:</strong> 2024 - Present</p>
+                        <h3>BE in Information Science</h3>
+                        <p>RV College of Engineering</p>
+                        <p>2024 - Present</p>
                     </div>
                 </div>
             </div>
